ea4648e6fc8b03646d11d6045ff9068c0d14865a
jonathanvgms <jonathanvgms@users.noreply.github.com>
2015-10-25 23:06:31 -0300

    Arreglo del logo.png

--- ffccSimulacion/Resultados/Informe.cs
+++ ffccSimulacion/Resultados/Informe.cs
@@ -121,7 +121,9 @@ namespace ffccSimulacion.Resultados
 
         private static Document generarEncabezado(Document doc, string nombreSimulacion, string fecha, string hora)
         {
+            //TODO aun no funciona el path del logo
             string path = Application.StartupPath + "\\Resources\\logo.png";
+            //string path = "C:\\\\Resources\\logo.png";
             Image logo = Image.GetInstance(new Uri(path));
             logo.ScalePercent(30);
             logo.Alignment = Image.TEXTWRAP | Image.ALIGN_LEFT;
